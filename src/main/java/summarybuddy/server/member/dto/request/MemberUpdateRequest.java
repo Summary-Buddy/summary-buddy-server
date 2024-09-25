e20f9d3d579f75aac20f92d6e6d1c26f2ddbb8f3
dlawlstn1616 <a01047181185@gmail.com>
2024-09-25 11:19:51 +0900

    MemberUpdateRequest create

---
+++ src/main/java/summarybuddy/server/member/dto/request/MemberUpdateRequest.java
@@ -0,0 +1,24 @@
+package summarybuddy.server.member.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberUpdateRequest {
+    @Email(message = "Email should be valid")
+    private String newEmail;
+
+    private String newPassword;
+    private String newPasswordConfirm;
+
+    public boolean checkPassword() {
+        return this.newPassword != null && this.newPassword.equals(this.newPasswordConfirm);
+    }
+
+    public boolean emailValidation() {
+        return this.newEmail != null && this.newEmail.contains("@");
+    }
+}
