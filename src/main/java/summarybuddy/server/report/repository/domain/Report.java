dc262e586e7aac7f93e79ebefcbaeda980902cf0
ywonchae1 <ywonchae62@gmail.com>
2024-09-26 15:08:11 +0900

    fix: 기본 생성자 추가

--- src/main/java/summarybuddy/server/report/repository/domain/Report.java
+++ src/main/java/summarybuddy/server/report/repository/domain/Report.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class Report {
     @Id
