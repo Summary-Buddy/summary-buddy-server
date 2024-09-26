4392863a1f134310206cb9a49387d86a58c25a8c
ywonchae1 <ywonchae62@gmail.com>
2024-09-26 13:00:12 +0900

    feat: 로그인 회원 정보 조회 애노테이션 추가

---
+++ src/main/java/summarybuddy/server/common/annotation/LoginMember.java
@@ -0,0 +1,12 @@
+package summarybuddy.server.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "username == null ? '' : username")
+public @interface LoginMember {}
