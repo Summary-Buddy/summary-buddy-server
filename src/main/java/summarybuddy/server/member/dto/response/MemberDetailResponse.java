5aabf2727fd767ebd7bd5116631dad9fcf27c334
ywonchae1 <ywonchae62@gmail.com>
2024-09-27 11:56:17 +0900

    chore: Spotless 적용

--- src/main/java/summarybuddy/server/member/dto/response/MemberDetailResponse.java
+++ src/main/java/summarybuddy/server/member/dto/response/MemberDetailResponse.java
@@ -4,13 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record MemberDetailResponse(
-        @NotNull
-        @Schema(description = "회원 ID", example = "1")
-        Long id,
-        @NotNull
-        @Schema(description = "회원 닉네임", example = "summary_buddy123")
-        String username,
-        @NotNull
-        @Schema(description = "회원 이메일", example = "summarybuddy@gmail.com")
-        String email) {
-}
+        @NotNull @Schema(description = "회원 ID", example = "1") Long id,
+        @NotNull @Schema(description = "회원 닉네임", example = "summary_buddy123") String username,
+        @NotNull @Schema(description = "회원 이메일", example = "summarybuddy@gmail.com")
+                String email) {}
