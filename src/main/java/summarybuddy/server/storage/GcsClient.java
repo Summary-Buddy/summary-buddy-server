c114c2d1fc68bb446b28734ab938b6d416bb91f0
ywonchae1 <ywonchae62@gmail.com>
2024-10-01 14:33:08 +0900

    chore: spotless 적용

--- src/main/java/summarybuddy/server/storage/GcsClient.java
+++ src/main/java/summarybuddy/server/storage/GcsClient.java
@@ -8,7 +8,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDateTime;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
